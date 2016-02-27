3b06ed9ed7ca78faad26677c8dcdc2ca68a15eac
Adam Leung <centribo@hotmail.com>
2016-02-27 11:26:09 -0500

    Updated Note.cs

--- Assets/Scripts/Note.cs
+++ Assets/Scripts/Note.cs
@@ -1,8 +1,15 @@
 ﻿using System.Collections;
 
 public class Note {
+
 	public float time;
 	public float holdTime;
 		//If holdTime = 0, then this note is just a "hit"
 		//Otherwise, holdTime represents how long the hold lasts for
+
+	//Constructor
+	public Note(float time, float HoldTime){
+		this.time = time;
+		this.holdTime = holdTime;
+	}
 }
