29a56ebfb3418d7432c1e9202880cf329c84afff
Adam Leung <centribo@hotmail.com>
2016-02-27 10:43:11 -0500

    Comments

--- Assets/Scripts/Note.cs
+++ Assets/Scripts/Note.cs
@@ -3,4 +3,6 @@
 public class Note {
 	public float time;
 	public float holdTime;
+		//If holdTime = 0, then this note is just a "hit"
+		//Otherwise, holdTime represents how long the hold lasts for
 }
