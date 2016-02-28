523d4801103b194c682d75314421b235436828db
xelendt <m@xelendt.com>
2016-02-28 04:49:28 -0500

    Updated to final-ish art

---
+++ Assets/Scripts/CalibrationManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CalibrationManager : MonoBehaviour {
+
+	public static CalibrationManager instance = null;
+	public static CalibrationManager Instance { //Singleton pattern instance
+		get { //Getter
+			if(instance == null){ //If its null,
+				instance = (CalibrationManager)FindObjectOfType(typeof(CalibrationManager)); //Find it
+			}
+			return instance; //Return it
+		}
+	}
+		
+	float runningTotal = 0;
+	int samples = 0;
+
+	public void TakeDelta(float delta){
+		runningTotal += delta;
+		samples++;
+	
+		//GameManager.Instance.Global_Offset = runningTotal / (float)samples;
+		GameManager.Instance.Total_Offset += delta;
+		GameManager.Instance.Offset_Samples++;
+	}
+}
---
+++ Assets/Scripts/EffectManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EffectManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+		// load all of the textures
+
+
+		// create all of the sprites and set defaults
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// swaps the image on the background
+	void moveBG() {
+
+	}
+
+	// draws a note and plays the sound based off the type of note
+	// also swaps the image on the notes
+	void drawNote() {
+
+	}
+
+	// plays a snare hit
+	void playSnare() {
+
+	}
+}
